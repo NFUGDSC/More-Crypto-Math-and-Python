2debf5a1206fde7a491aa463a86a56a4fbfc7778
NFU GDSC <92938908+NFUGDSC@users.noreply.github.com>
2021-11-23 14:13:36 +0800

    Create Euler_Theorem.py

---
+++ Euler_Theorem.py
@@ -0,0 +1,16 @@
+def gcd(a,b):
+    if not b:
+        return a
+    return gcd(b,a%b)
+
+def phi(n):
+    x=0
+    for i in range(n):
+        if gcd(i,n)==1:
+            x+=1
+    return x
+
+def euler(a, n):
+    x = phi(n)
+    return pow(a,x-1,n)#inverse
+
