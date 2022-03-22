d4aad38f0f03e6935c33250efade843761e0cc42
NFU GDSC <92938908+NFUGDSC@users.noreply.github.com>
2022-03-22 14:55:48 +0800

    Update long_to_bytes.py

--- long_to_bytes.py
+++ long_to_bytes.py
@@ -1,6 +1,5 @@
-def long_to_bytes(value, length):
-    result = []
-    for i in range(0, length):
-        result.append(value >> (i * 8) & 0xff)
-    result.reverse()
-    return result
+def long_to_bytes(long):
+    result = ""
+    for i in range(len(str(long))):
+        result+=(chr(long >> (i * 8) & 0xff))
+    return result[::-1]
